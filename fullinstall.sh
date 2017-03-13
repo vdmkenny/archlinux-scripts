a2936c205608b5ab6a32d6d0c65db605effe70ca
Kenny Van de Maele <vdmkenny@inuits.eu>
2017-03-13 13:53:38 +0100

    added bootloader install
    Signed-off-by: Kenny Van de Maele <vdmkenny@inuits.eu>

--- fullinstall.sh
+++ fullinstall.sh
@@ -23,6 +23,7 @@ then
 fi
 
 #check if install device exists
+echo "Checking for disk ${installdisk}"
 if ! ls ${installdisk}; then
     echo "${installdisk} not found. Exiting."
     exit 1
@@ -39,9 +40,11 @@ else
 fi
 
 #Enable NTP
+echo "Enabling NTP"
 timedatectl set-ntp true
 
 #format disk and partition
+echo "Partitioning disk ${installdisk}"
 if $EFI; then
 fdisk ${installdisk} <<EOF
 g
@@ -57,13 +60,13 @@ n
 
 w
 EOF
-
+echo "Formatting ESP"
 mkfs.fat -F32 /dev/sda1
 
 else
 fdisk ${installdisk} <<EOF
-g
 n
+p
 1
 
 
@@ -73,6 +76,7 @@ EOF
 fi
 
 #create LVM volume
+echo "Creating LVM volume 'system'"
 if $EFI; then
 	pvcreate ${installdisk}2
 	vgcreate system ${installdisk}2
@@ -80,23 +84,29 @@ else
 	pvcreate ${installdisk}1
 	vgcreate system ${installdisk}1
 fi
+echo 'Creating home and root volumes'
 lvcreate -L ${homesize} system -n lvm-home
 lvcreate -l +100%FREE system -n lvm-root 
 
-#partition disks
+#format disks
+echo "Formatting root/home LVs to XFS"
 mkfs.xfs /dev/mapper/system-lvm--root
 mkfs.xfs /dev/mapper/system-lvm--home
 
 #edit mkinitcpio hooks for lvm boot
+echo "Editing mkinitcpio hooks for LVM boot"
 sed -i "/^HOOKS/c\HOOKS\= \"base udev autodetect modconf block lvm2 filesystems keyboard fsck\"/" /etc/mkinitcpio.conf
 
 #set mirrorlist to kangaroot
+echo "Setting mirrorlist to kangaroot"
 echo 'Server = http://archlinux.mirror.kangaroot.net/$repo/os/$arch' > /etc/pacman.d/mirrorlist
 
 #create mountpoints
+echo "Creating mountpoints"
 mkdir -p /mnt /mnt/boot /mnt/home
 
 #mount disks
+echo "Mounting disks"
 mount /dev/mapper/system-lvm--root /mnt
 mount /dev/mapper/system-lvm--home /mnt/home
 if ! $EFI; then
@@ -104,34 +114,81 @@ if ! $EFI; then
 fi
 
 #pacstrap all the things
+echo "Pacstrapping root"
 pacstrap /mnt base base-devel gnome gnome-tweak-tool pwgen zsh ${extrapackages}
 
 #generate fstab file
+echo "Generating fstab"
 genfstab -U /mnt > /mnt/etc/fstab
 
 #chroot into new fs
+echo "Chrooting to new root"
 arch-chroot /mnt
 
 #set timezone
+echo "Setting timezone to Brussels"
 ln -sf /usr/share/zoneinfo/Europe/Brussels /etc/localtime
 
 #set hardware clock
+echo "Setting hardware clock"
 hwclock --systohc
 
 #set locale
+echo "Generating locales"
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 echo "nl_BE.UTF-8 UTF-8" >> /etc/locale.gen
 echo "fr_BE.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 
 #set hostname
+echo "Setting hostname to ${hostname}"
 echo ${hostname} > /etc/hostname
 
 #double tap initramfs to be sure
+echo "Re-generating initramfs"
 mkinitcpio -p linux
 
 #set rootpw to something random
+echo "Randomizing root password"
 randompw=$(pwgen -c -n -y | head -n 1)
 echo root:${randompw} | chpasswd
 
-useradd -m -d /home/${username}/ -s /bin/zsh -g wheel ${username}
+#add defined user
+echo "Adding user ${username}"
+useradd -m -d /home/${username}/ -s /bin/zsh -G wheel ${username}
+echo ${username}:${userpass} | chpasswd
+
+#install bootloader, systemdboot for EFI, grub for BIOS.
+if $ESP; then
+    echo "Installing systemdboot"
+    bootctl --path=/boot install
+    bootfile="default  arch
+timeout  4
+editor   0"
+    echo ${bootfile} > /boot/loader/loader.conf
+    
+    archloader="title          Arch Linux
+linux          /vmlinuz-linux
+initrd         /initramfs-linux.img
+options        root=/dev/mapper/system-lvm--root rw"
+    echo ${archloader} > /boot/loader/entries/arch.conf
+
+   
+else
+    echo "Installing GRUB"
+    pacman -S grub --noconfirm
+    grub-install --target=i386-pc ${installdisk}
+    grub-mkconfig -o /boot/grub/grub.cfg
+fi
+
+echo "Enabling GDM"
+systemctl enable gdm
+
+echo "Exiting chroot environment"
+exit
+
+echo "Unmounting partitions"
+umount -R /mnt
+
+echo "Rebooting NOW"
+reboot now
